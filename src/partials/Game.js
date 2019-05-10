b0d284f4d9a6860cb4c301cd302268fdf54ab326
EdinK1 <edinkaymakchi@gmail.com>
2019-05-09 20:29:59 -0700

    build game with no stretch goals

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,7 +1,8 @@
-import {SVG_NS, PADDLE_WIDTH, PADDLE_HEIGHT, BOARD_GAP, KEYS} from '../settings';
+import {SVG_NS, PADDLE_WIDTH, PADDLE_HEIGHT, BOARD_GAP, KEYS, RADIUS} from '../settings';
 import Board from './Board';
 import Paddle from './Paddle';
 import Ball from './Ball';
+import Score from './Score';
 
 export default class Game {
   constructor(element, width, height) {
@@ -17,12 +18,12 @@ export default class Game {
 
     const paddle2Gap = this.width - BOARD_GAP - PADDLE_WIDTH;
     this.paddle2 = new Paddle(this.height, PADDLE_WIDTH, PADDLE_HEIGHT, paddle2Gap, boardMid, KEYS.p2up, KEYS.p2down);
-    this.ball = new Ball(this.width, this.height);
-		// Other code goes here...
+    this.ball = new Ball(this.width, this.height, RADIUS);
+    this.score1 = new Score(this.width / 2 - 50, 30);
+    this.score2 = new Score(this.width / 2 + 25, 30);
   }
 
   render() {
-    // More code goes here
     this.gameElement.innerHTML = '';
     let svg = document.createElementNS(SVG_NS, 'svg');
     svg.setAttributeNS(null, 'width', this.width);
@@ -32,7 +33,9 @@ export default class Game {
     this.board.render(svg);
     this.paddle1.render(svg);
     this.paddle2.render(svg);
-    this.ball.render(svg)
+    this.ball.render(svg, this.paddle1, this.paddle2);
+    this.score1.render(svg, this.paddle1.getScore());
+    this.score2.render(svg, this.paddle2.getScore());
     
   // 
   }
--- src/partials/Paddle.js
+++ src/partials/Paddle.js
@@ -9,7 +9,48 @@ export default class Paddle {
     this.y = initialY;
     this.score = 0;
     this.speed = SPEED;
+    document.addEventListener('keydown', event => {
+        switch(event.key) {
+            case keyUp: 
+                this.moveUp();
+                break;
+            case keyDown:
+                this.moveDown();
+                break;
+        }
+        });
     }
+
+    increaseScore() {
+        this.score++;
+    }
+
+    getScore() {
+        return this.score;
+    }
+
+    resetScore() {
+        this.score  = 0;
+    }
+
+    moveUp() {
+        this.y = Math.max(10, this.y - this.speed);
+    }
+
+    moveDown() {
+        this.y = Math.min(this.boardHeight - this.paddleHeight - this.speed, this.y + this.speed);
+    }
+
+    getCoordinates() {
+        const walls = {
+            left: this.x,
+            top: this.y,
+            right: this.x + this.paddleWidth,
+            bottom: this.y + this.paddleHeight
+        }
+        return walls;
+    }
+
     render(svg) {
         let rect = document.createElementNS(SVG_NS, 'rect');
         rect.setAttributeNS(null, 'width', this.paddleWidth);
